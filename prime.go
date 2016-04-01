d3b2336a9e080faf7f9830536f1f97e42f5aebea
Micke <sstrunks13@gmail.com>
2016-04-01 21:50:33 +0200

    Added isSGPrime(), with test, to check if a number is a Sophie Germain prime

--- prime.go
+++ prime.go
@@ -8,3 +8,10 @@ func isSafePrime(i *big.Int) bool {
 	q.Div(q, big.NewInt(2))
 	return i.ProbablyPrime(40) && q.ProbablyPrime(40)
 }
+
+func isSGPrime(i *big.Int) bool {
+	q := big.NewInt(32)
+	q.Mul(i, big.NewInt(2))
+	q.Add(q, big.NewInt(1))
+	return i.ProbablyPrime(40) && q.ProbablyPrime(40)
+}
